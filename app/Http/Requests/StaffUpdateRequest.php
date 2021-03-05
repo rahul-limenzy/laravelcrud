00f34678e4074a538caac40ae28b070d056124ee
Rahul R <rahul.limenzy@gmail.com>
2021-03-05 10:26:46 +0530

    validation done

---
+++ app/Http/Requests/StaffUpdateRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StaffUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $staff = $this->route('staff');
+        return [
+            'first_name' => 'required|max:255',
+            'last_name' => 'required|max:255',
+            'address' => 'required|max:255',
+            'email' => ['required', Rule::unique('staff')->ignore($staff->id)]
+        ];
+    }
+}
